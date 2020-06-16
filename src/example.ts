d015ef6677b743024dd24d024edadb760d32ca25
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-06-16 15:59:06 +0100

    Update example

--- src/example.ts
+++ src/example.ts
@@ -2,17 +2,20 @@ import * as T from './types'
 
 const test = T.object({
   /** prop docs go here */
-  prop: T.literal([true, 'string']),
+  prop: T.union(T.boolean(), T.optional(T.string())),
   /** prop2 docs, nice */
   prop2: T.readonly(T.optional(T.readonly(T.boolean()))),
 
   prop3: T.record(T.optional(T.string())),
+
+  prop4: T.function(T.string(), T.boolean(), T.number()),
 })
 
 export type Test = T.TypeOf<typeof test>
 
 export const t: Test = {
-  prop: [true, 'string'],
+  prop: true,
   prop2: true,
   prop3: {prop: undefined},
+  prop4: () => 123,
 }
